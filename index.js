0316fccc36e85fc8c13d01b09f0edfbd173148b8
Hassan Hashmi <hashmihassan47@gmail.com>
2023-01-20 15:29:57 -0500

    added default word of word and basic text box to guess to game page

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-//displays the game screen
+//open game.html
 function playClicked(){
   window.location.pathname = "/html/game.html";
+}
+
+function submitGuess() {
+  document.getElementsByClassName('.guessBox').text ='';
 }
