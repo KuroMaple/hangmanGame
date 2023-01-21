a14643592c50ecb7cedb351c428403e87bfaa8c3
Hassan Hashmi <hashmihassan47@gmail.com>
2023-01-21 00:54:35 -0500

    added array and fixing those related issues

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+var testText = ["w", "o", "r", "d"]; // hardcoded dummy word
+var guessedText = ["_" , "_", "_", "_"];
+
 //open game.html
 function playClicked(){
   window.location.pathname = "/html/game.html";
+  var displayText = "";
+  for (let i = 0; i < 4; i++){ //hardcoding for loop for hard coded word
+    displayText = displayText + " " + guessedText[i]; //generates the current dash sequence
+  }
+  document.getElementById('wordField').innerHTML = displayText; //reflecting sequence in display
 }
 
 //Clears the textfield and adds to the user's guess
