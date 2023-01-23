6baca5540c474f90143ff741cacbd8e38acd990b
Hassan Hashmi <hashmihassan47@gmail.com>
2023-01-23 11:00:16 -0500

    trying to get getElementById working

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ var guessedText = ["_" , "_", "_", "_"];
 //open game.html
 function playClicked(){
   window.location.pathname = "/html/game.html";
-  var displayText = "";
-  for (let i = 0; i < 4; i++){ //hardcoding for loop for hard coded word
-    displayText = displayText + " " + guessedText[i]; //generates the current dash sequence
-  }
-  document.getElementById('wordField').innerHTML = displayText; //reflecting sequence in display
+}
+
+function updateText(){
+  document.getElementById("wordField").textContent = "Word";
 }
 
 //Clears the textfield and adds to the user's guess
